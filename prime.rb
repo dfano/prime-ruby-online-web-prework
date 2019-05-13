e39a6b7faff23917e34fb6914b763b857391c699
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-13 14:54:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+	if num <= 0
+		false
+	elsif num == 1 || num == 2
+		true
+	elsif
+		for i in (2..(num-1))
+	    	# return false if num % i == 0
+	    	if i % num == 0
+	    		return false
+	    	else
+	    		return true
+	    	end
+	  	end
+	end
+end
